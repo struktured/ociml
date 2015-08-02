39e771653e2088e22dbb2f4f1798204b7f0243d5
carm <carmelo.piccione@gmail.com>
2015-08-02 02:19:10 -0400

    remove unused some/none c defs

--- oci_types.c
+++ oci_types.c
@@ -12,19 +12,6 @@
 #include <oci.h>
 #include "oci_wrapper.h"
 
-#define Val_none Val_int(0)
-
-static value
-Val_some( value v )
-{   
-    CAMLparam1( v );
-    CAMLlocal1( some );
-    some = caml_alloc(1, 0);
-    Store_field( some, 0, v );
-    CAMLreturn( some );
-}
-
-
 /* convert an epoch time to an Oracle date */
 void epoch_to_ocidate(double e, OCIDate* ocidate) {
   time_t t = (time_t)e;
